49ef9a3aebff745a74b11c1ea2ee49ccf9d6903f
root <501845230@qq.com>
2017-11-08 18:23:10 +0800

    add urls and base_food file

--- df_user/urls.py
+++ df_user/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^register/', views.register),
+    url(r'^register_handle/', views.register_handle),
+    url(r'^login/', views.login),
+    url(r'^login_handle/', views.login_handle),
+    url(r'^info/', views.user_center_info),
 ]
